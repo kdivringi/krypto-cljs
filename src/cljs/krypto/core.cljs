cbf5d4cff6a8a15f5fcfa45117646aeda84e7ff9
Kaan Divringi <kdivringi@gmail.com>
2017-02-25 21:51:17 -0800

    I do not understand how the om query and normalization stuff work

--- src/cljs/krypto/core.cljs
+++ src/cljs/krypto/core.cljs
@@ -14,9 +14,12 @@
       {:value :not-found})))
 
 (defn mutate [{:keys [state] :as env} key params]
-  (if (= 'increment key)
-    {:value {:keys [:count]}
-     :action #(swap! state update-in [:count] inc)}
+  (if (= 'krypto.core/play key)
+    {:value nil
+     :action #(let [new-board (conj (:board @state) params)
+                    new-cards (remove (partial = params) (:cards @state))]
+                (reset! state {:cards new-cards :board new-board})
+                )}
     {:value :not-found}))
 
 (defui ^:once Counter
@@ -37,14 +40,24 @@
 
 ; I don't really want to create this but I can't find another way to specify the React Key
 (defui PlayCard
+  static om/IQuery
+  (query [this]
+         '[:value])
   Object
   (render [this]
           (let [{:keys [value] :as c} (om/props this)]
-            (dom/li #js {:className "card"} value))))
+            (dom/li #js {:className "card"
+                         :onClick
+                         (fn [e]
+                           (om/transact! this `[(play ~c)]))}
+                    value))))
 
 (def playcard (om/factory PlayCard {:keyfn :value}))
 
 (defui Cards
+  static om/IQuery
+  (query [this]
+         [:cards])
   Object
   (render [this]
           (apply dom/ul #js {:className "hlist"}
@@ -53,13 +66,36 @@
 
 (def cards (om/factory Cards))
 
+(defui BoardCard ; Super similar to playcard except for action...
+  static om/IQuery
+  (query [this]
+         '[:board])
+  Object
+  (render [this]
+          (let [{:keys [value] :as c} (om/props this)]
+            (dom/li #js {:className "card"
+                         :onClick
+                         (fn [e]
+                           (om/transact! this `[remove ~c]))} value))))
+
+(def boarditem (om/factory BoardCard {:keyfn :value}))
+
+(defui Board
+  Object
+  (render [this]
+          (apply dom/ul #js {:className "hlist"}
+                 (map boarditem (om/props this)))))
+
+(def boardview (om/factory Board))
+
 (defui ^:once App
   Object
   (render [this]
           (dom/div #js {:className "App"}
                    (dom/h1 #js {:className "Title"} "Krypto!")
                    (dom/p #js {:className "Lead"} "Use the cards")
-                   (cards (:cards (om/props this))))))
+                   (dom/div nil (cards (:cards (om/props this))))
+                   (dom/div nil (boardview (:board (om/props this)))))))
 
 (defonce reconciler
   (om/reconciler {:state app-state
@@ -70,6 +106,6 @@
   (if (nil? @app-state)
     (let [target (gdom/getElement "app")]
       (om/add-root! reconciler App target)
-      (reset! app-state {:cards static_cards :board []}))
+      (reset! app-state {:cards (rest static_cards) :board [(first static_cards)]}))
     (.forceUpdate (om/class->any reconciler App))))
 
