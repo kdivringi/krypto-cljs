46c6329cab45822fb14d0b34f3c4de27f4616fc1
Kaan Divringi <kdivringi@gmail.com>
2017-02-26 09:26:52 -0800

    Am I crazy or does the components-identity-normalization tutorial code not work?

--- src/cljs/krypto/core.cljs
+++ src/cljs/krypto/core.cljs
@@ -3,24 +3,70 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+(enable-console-print!)
+
+(def init-data
+  {:list/one [{:name "John" :points 0}
+              {:name "Mary" :points 0}
+              {:name "Bob"  :points 0}]
+   :list/two [{:name "Mary" :points 0 :age 27}
+              {:name "Gwen" :points 0}
+              {:name "Jeff" :points 0}]})
+
+(defmulti read om/dispatch)
+
+(defn get-people [state key]
+  (let [st @state]
+    (into [] (map #(get-in st %)) (get st key))))
+
+(defmethod read :list/one
+  [{:keys [state] :as env} key params]
+  {:value (get-people state key)})
+
+(defmethod read :list/two
+  [{:keys [state] :as env} key params]
+  {:value (get-people state key)})
+
+(defui Person
+  static om/Ident
+  (ident [this {:keys [name]}]
+         [:person/by-name name])
+  static om/IQuery
+  (query [this]
+         '[:name :points]))
+
+(defui RootView
+  static om/IQuery
+  (query [this]
+         (let [subquery (om/get-query Person)]
+           `[{:list/one ~subquery} {:list/two ~subquery}])))
+
+(defmulti mutate om/dispatch)
+
+(defmethod mutate 'points/increment
+  [{:keys [state]} _ {:keys [name]}]
+  {:action
+   (fn []
+     (swap! state update-in
+            [:person/by-name name :points]
+            inc))})
+
+(defmethod mutate 'points/decrement
+  [{:keys [state]} _ {:keys [name]}]
+  {:action
+   (fn []
+     (swap! state update-in
+            [:person/by-name name :points]
+            #(let [n (dec %)] (if (neg? n) 0 n))))})
 
 (def static_cards [{:value 3} {:value 5} {:value 10} {:value 4} {:value 1}])
 
 (def app-state (atom {:cards static_cards :board []}))
 
-(defn read [{:keys [state] :as env} key params]
-  (let [st @state]
-    (if-let [[_ value] (find st key)]
-      {:value value}
-      {:value :not-found})))
+;; (defn read [{:keys [state] :as env} key params]
+;;   (let [st @state]
+;;     {:value (key st)}))
 
-(defn mutate [{:keys [state] :as env} key params]
-  (if (= 'krypto.core/play key)
-    {:action #(let [new-board (conj (:board @state) params)
-                    new-cards (into [] (remove (partial = params) (:cards @state)))]
-                (reset! state {:cards new-cards :board new-board})
-                )}
-    {:value :not-found}))
 
 (defn play-card [value]
   (let [new-board (conj (:board @app-state) value)
